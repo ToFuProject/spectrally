22bfa90a040bc20eb42fbc41b2fbf989fcd254d0
dvezinet <didier.vezinet@gmail.com>
2024-02-11 23:23:22 +0100

    [#devel] updated spectrally/entrypoints/_def.py done

--- spectrally/entrypoints/_def.py
+++ spectrally/entrypoints/_def.py
@@ -1,34 +1,14 @@
 
 
 # #############################################################################
-#                   tofuplot parameters
+#                   Parameters
 # #############################################################################
 
-_TFPLOT_CONFIG = None
-_TFPLOT_TOSOLEDGE3X = False
-_TFPLOT_RUN = 0
-_TFPLOT_USER = None
-_TFPLOT_DATABASE = None
-_TFPLOT_VERSION = None
-_TFPLOT_T0 = None
-_TFPLOT_SHAREX = False
-_TFPLOT_BCK = True
-_TFPLOT_EXTRA = True
-_TFPLOT_INDCH_AUTO = True
-_TFPLOT_MAG_SEP_NBPTS = 5
-_TFPLOT_MAG_SEP_DR = 0.
-_TFPLOT_MAG_INIT_PTS = None
+
 
 
 # #############################################################################
-#                   tofucalc parameters
+#                   Parameters
 # #############################################################################
-_TFCALC_RUN = 0
-_TFCALC_USER = None
-_TFCALC_DATABASE = None
-_TFCALC_VERSION = None
-_TFCALC_T0 = None
-_TFCALC_SHAREX = False
-_TFCALC_BCK = True
-_TFCALC_EXTRA = None
-_TFCALC_INDCH_AUTO = True
+
+
