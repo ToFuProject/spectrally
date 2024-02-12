3a9d0d8fab2116e2e59e69773bcae6ca4b794e2c
dvezinet <didier.vezinet@gmail.com>
2024-02-12 02:13:46 +0100

    [#devel] Minor clean-up

--- spectrally/openadas/__init__.py
+++ spectrally/openadas/__init__.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/python
 """
-The openadas-compatibility module of tofu
+The openadas-compatibility module of spectrally
 
 """
+
+
 import traceback
+
 
 try:
     try:
-        from tofu.openadas2tofu._requests import *
-        from tofu.openadas2tofu._read_files import *
+        from spectrally.openadas._requests import *
+        from spectrally.openadas._read_files import *
     except Exception:
         from ._requests import *
         from ._read_files import *
+
 except Exception as err:
     msg = str(traceback.format_exc())
-    msg += "\n\n    => the optional sub-package tofu.imas2tofu is not usable\n"
+    msg += "\n\n\t=> optional sub-package spectrally.openadas not usable\n"
     raise Exception(msg)
 
 del traceback
