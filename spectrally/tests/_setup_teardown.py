ed588a8000a7376b4dd9f17376c6f8a5e376c7b7
dvezinet <didier.vezinet@gmail.com>
2024-02-20 03:16:31 +0100

    [#devel] fixed unit tests 5

--- spectrally/tests/_setup_teardown.py
+++ spectrally/tests/_setup_teardown.py
@@ -19,30 +19,15 @@ __all__ = ['setup_module0', 'teardown_module0']
 
 
 _PATH_HERE = os.path.dirname(__file__)
-_PATH_INPUT = os.path.join(_PATH_HERE, 'input')
 _PATH_OUTPUT = os.path.join(_PATH_HERE, 'output')
 
 
 _PKG = 'spectrally'
 _PATH_SP = os.path.join(os.path.expanduser('~'), f'.{_PKG}')
-_PATH_OPAD = os.path.join(_PATH_SP, 'openadas')
-_PATH_NIST = os.path.join(_PATH_SP, 'nist')
 
 
 _CUSTOM = os.path.dirname(os.path.dirname(os.path.dirname(_PATH_HERE)))
 _CUSTOM = os.path.join(_CUSTOM, 'scripts', 'tofucustom.py')
-
-
-_CLEAN = False
-if not os.path.isdir(_PATH_SP):
-    os.mkdir(_PATH_SP)
-    _CLEAN = True
-if not os.path.isdir(_PATH_OPAD):
-    os.mkdir(_PATH_OPAD)
-    _CLEAN = True
-if not os.path.isdir(_PATH_NIST):
-    os.mkdir(_PATH_NIST)
-    _CLEAN = True
 
 
 #######################################################
@@ -64,21 +49,20 @@ def clean_output(path=_PATH_OUTPUT):
             os.remove(os.path.join(path, ff))
 
 
-def create_local_path(clean=_CLEAN):
-    if clean is True:
-        os.system(f'python {_CUSTOM}')
+def create_local_path():
 
-
-def clean_local_path(clean=_CLEAN):
-    if clean is True and os.path.isdir(_PATH_SP):
+    # clean
+    if os.path.isdir(_PATH_SP):
         shutil.rmtree(_PATH_SP)
+
+    # re-create
+    os.system(f'python {_CUSTOM}')
 
 
 def setup_module0(module):
-    clean_output()
     create_local_path()
+    clean_output()
 
 
 def teardown_module0(module):
     clean_output()
-    clean_local_path()
