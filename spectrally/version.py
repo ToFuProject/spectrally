60a73e83b1440e45f9731bc68c60a55e74724096
dvezinet <dvezinet@cfs.energy>
2024-07-30 14:59:39 +0000

    [#27] fixed version.py

--- spectrally/version.py
+++ spectrally/version.py
@@ -1,2 +1,2 @@
-# Do not edit, pipeline versioning governed by git tags!
-__version__ = '0.0.2-21-gf34bf7a'
+# Do not edit, pipeline versioning governed by git tags!
+__version__ = '0.0.2-24-gb9b2274'
